19eb201c9a449c64e99c1a7a76cd1f77334f6f4b
Charles Martin Reed <charlesmartinreed@icloud.com>
2019-01-01 19:34:03 -0600

    Recreation of the famous Star Wars crawl.

--- UIViews in 3D/ViewController.swift
+++ UIViews in 3D/ViewController.swift
@@ -11,50 +11,37 @@ import UIKit
 class ViewController: UIViewController {
 
     //MARK:- Properties
+    var openingCrawl: UILabel!
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
         view.backgroundColor = .black
         
-        //creating our cube, face by face
-        let cube = CATransformLayer()
+        //attributed strings for the crawl styling, title and text
+//        let titleParagraphStyle = NSMutableParagraphStyle()
+//        titleParagraphStyle.alignment = .center
+//
+//        let textParagraphStyle = NSMutableParagraphStyle()
+//        textParagraphStyle.alignment = .justified
+//
+//        let titleAttributes = [
+//            NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 32),
+//            NSAttributedString.Key.paragraphStyle: titleParagraphStyle
+//        ]
+//        let textAttributes = [
+//            NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 22),
+//            NSAttributedString.Key.paragraphStyle: textParagraphStyle
+//        ]
         
-        //front face
-        let transform1 = CATransform3DMakeTranslation(0, 0, 50)
-        cube.addSublayer(face(with: transform1, color: .red))
+        let title = "EPISODE LLVM\nSWIFT EVOLUTION\n"
+        let body = "It is a period of civil war. People BIKESHED proposals while Swift itself still hasn't finished compiling code written during the OLD REPUBLIC.\n\nXCODE crashes faster than a speeder bike on Endor, but undeterred Apple unleashed a new wave of Auto Layout problems with THE NOTCH.\n\nMeanwhile, the GALACTIC EMPI — er, GOOGLE — are building an army of clones using Java Kotlin Flutter. Now all hopes for the REBEL ALLIANCE lie with Swift's developers, who must add features, fix bugs, and, most importantly, prepare t— ERROR Assertion failed: (hasInterfaceType() && \"No interface type was set\"), function getInterfaceType, file /Users/Crusty/workspace/swift/AST/Decl.cpp line 1977."
         
-        //create the right-hand face
-        var transform2 = CATransform3DMakeTranslation(50, 0, 0)
-        transform2 = CATransform3DRotate(transform2, CGFloat.pi / 2, 0, 1, 0)
-        cube.addSublayer(face(with: transform2, color: .yellow))
+        makeAttributedText(title: title, body: body)
+        //let title = NSMutableAttributedString(string: "EPISODE LLVM\nSWIFT EVOLUTION\n", attributes: titleAttributes)
+        //let text = NSAttributedString(string: mainText, attributes: textAttributes)
+        //title.append(text)
         
-        //create the top face
-        var transform3 = CATransform3DMakeTranslation(0, -50, 0)
-        transform3 = CATransform3DRotate(transform3, CGFloat.pi / 2, 1, 0, 0)
-        cube.addSublayer(face(with: transform3, color: .green))
-        
-        //create the bottom face
-        var transform4 = CATransform3DMakeTranslation(0, 50, 0)
-        transform4 = CATransform3DRotate(transform4, -(CGFloat.pi / 2), 1, 0, 0)
-        cube.addSublayer(face(with: transform4, color: .white))
-        
-        //create the left-hand face
-        var transform5 = CATransform3DMakeTranslation(-50, 0, 0)
-        transform5 = CATransform3DRotate(transform5, -(CGFloat.pi / 2), 0, 1, 0)
-        cube.addSublayer(face(with: transform5, color: .cyan))
-        
-        var transform6 = CATransform3DMakeTranslation(0, 0, -50)
-        transform6 = CATransform3DRotate(transform6, CGFloat.pi, 0, 1, 0)
-        cube.addSublayer(face(with: transform6, color: .magenta))
-        
-        // position the cube transform layer in the center
-        cube.position = CGPoint(x: view.bounds.midX, y: view.bounds.midY)
-        
-        //add the cube to the view's layers
-        view.layer.addSublayer(cube)
-        
-        //now let's animate the cube!
-        rotate(layer: cube)
+
     }
     
     override func viewDidLoad() {
@@ -62,30 +49,74 @@ class ViewController: UIViewController {
         
     }
     
-    //MARK:- transformation methods
-    
-    func face(with transform: CATransform3D, color: UIColor) -> CALayer {
-        //this method is responsible for creating a single face of the cube
-        let face = CALayer()
+    fileprivate func makeAttributedText(title: String, body: String) {
+        //attributed strings for the crawl styling, title and text
+        let titleParagraphStyle = NSMutableParagraphStyle()
+        titleParagraphStyle.alignment = .center
         
-        face.frame = CGRect(x: -50, y: -50, width: 100, height: 100)
-        face.backgroundColor = color.cgColor
-        face.transform = transform
+        let textParagraphStyle = NSMutableParagraphStyle()
+        textParagraphStyle.alignment = .justified
         
-        return face
+        let titleAttributes = [
+            NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 32),
+            NSAttributedString.Key.paragraphStyle: titleParagraphStyle
+        ]
+        let bodyAttributes = [
+            NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 22),
+            NSAttributedString.Key.paragraphStyle: textParagraphStyle
+        ]
+        
+        let scrollTitle = NSMutableAttributedString(string: title, attributes: titleAttributes)
+        let scrollBody = NSAttributedString(string: body, attributes: bodyAttributes)
+        scrollTitle.append(scrollBody)
+        
+        makeCrawlLabelFromMutableAttributedText(scrollable: scrollTitle)
     }
+    
+    fileprivate func makeCrawlLabelFromMutableAttributedText(scrollable: NSMutableAttributedString) {
+        openingCrawl = UILabel()
+        openingCrawl.translatesAutoresizingMaskIntoConstraints = false
+        openingCrawl.attributedText = scrollable
+        openingCrawl.textColor = UIColor(red: 250/255.0, green: 226/255.0, blue: 83/255.0, alpha: 1)
+        openingCrawl.numberOfLines = 0
+        
+        //label should fit the screen, with minimum space at the edges
+        let labelSize = openingCrawl.sizeThatFits(CGSize(width: view.bounds.width - 20, height: CGFloat.greatestFiniteMagnitude))
+        openingCrawl.frame = CGRect(x: 10 - (view.bounds.width / 2), y: 0, width: labelSize.width, height: labelSize.height)
+        
+        let layer = CATransformLayer()
+        layer.position = CGPoint(x: view.bounds.midX, y: view.bounds.midY)
+        
+        var perspective = CATransform3DIdentity
+        perspective.m34 = -1 / 250
+        layer.transform = perspective
+        
+        layer.addSublayer(openingCrawl.layer)
+        view.layer.addSublayer(layer)
+        
+        //rotating then translating != translating then rotating
+        let crawlTransformStart = CATransform3DMakeRotation(0.5, 1, 0, 0)
+        openingCrawl.layer.transform = CATransform3DTranslate(crawlTransformStart, 0, 400, 0)
+        
+        animateUpIntoVanishingPoint(layer: openingCrawl.layer)
+        
+    }
+    
+    //MARK:- transformation methods
+    func animateUpIntoVanishingPoint(layer: CALayer) {
+        let anim = CABasicAnimation(keyPath: "transform")
+        anim.fromValue = layer.transform
+        
+        let crawlTransformEnd = CATransform3DMakeRotation(0.5, 1, 0, 0)
+        anim.toValue = CATransform3DTranslate(crawlTransformEnd, 0, -500, 0)
+        
+        anim.duration = 90
+        layer.add(anim, forKey: "tranform")
+    }
+    
+    
     
     //MARK:- animation methods
-    func rotate(layer: CATransformLayer) {
-        let anim = CABasicAnimation(keyPath: "transform")
-        
-        anim.fromValue = layer.transform
-        anim.toValue = CATransform3DMakeRotation(CGFloat.pi, 1, 1, 1)
-        anim.duration = 2
-        anim.isCumulative = true //keep the cube rotating indefinitely by allowing one animation cycles last value to persistent into the next cycle
-        anim.repeatCount = .greatestFiniteMagnitude
-        layer.add(anim, forKey: "transform")
-    }
 
 
 }
