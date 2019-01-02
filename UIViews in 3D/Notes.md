e1a6b4bfa6f654fd79109abcb703cc9c298fb0f3
Charles Martin Reed <charlesmartinreed@icloud.com>
2019-01-01 18:36:25 -0600

    Implement simple rocking animation for a CALayer, along the X axis.

---
+++ UIViews in 3D/Notes.md
@@ -0,0 +1,8 @@
+#  <#Title#>
+
+Other Core Animation provided UIViews
+**CATiledLayer** (seen in Maps, for example), **CAEmitterLayer** (particles)
+
+For CATransform3D, altering the matricies allowes you to alter the strength of the 3D effect by affecting the distance between your eye and where the scene is drawn in the screen. 
+For a 4x4 matrix, for example (m11, m12, m21, m22, m31, m32, m41, m42, etc...)
+In this example, we might set the value for m34 to -1  / 500, which has a strong 3D effect. But smaller values like -1/2000 would be weaker.
--- UIViews in 3D/ViewController.swift
+++ UIViews in 3D/ViewController.swift
@@ -10,9 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK:- Properties
+    //we'll be rendering this image view in 3D space, we need a strong reference to it to keep it alive through the lifetime of the app
+    let imageView = UIImageView(frame: CGRect(x: -150, y: -150, width: 300, height: 300))
+    let perspectiveShift: CGFloat = -1 / 500
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        view.backgroundColor = .black
+        
+        //load an image into the imageView
+        imageView.image = UIImage(named: "flowers")
+        imageView.contentMode = .scaleAspectFit
+        
+        //create the transform layer
+        let transformLayer = CATransformLayer()
+        var perspective = CATransform3DIdentity //gives us a 4x4 matrix
+        
+        perspective.m34 = perspectiveShift
+        transformLayer.transform = perspective
+        
+        //to make the 3D effect work correctly in this use case, we need our transformLayer to be centered on the screen
+        transformLayer.position = CGPoint(x: view.bounds.midX, y: view.bounds.midY)
+        
+        //add the image view layer to the transform layer, and the transform layer to the main view itself
+        transformLayer.addSublayer(imageView.layer)
+        view.layer.addSublayer(transformLayer)
+        
+        //a simple a rotation transform for our image - not using this one!
+        //imageView.layer.transform = CATransform3DMakeRotation(-0.5, 1, 0, 0)
+        
+        //animation #1: rocking back and forth, along the x axis
+        make3DAnimation(layer: imageView.layer)
+    }
+    
+    //MARK:- Layer animations
+    fileprivate func make3DAnimation(layer: CALayer) {
+        let anim = CABasicAnimation(keyPath: "transform")
+        anim.fromValue = CATransform3DMakeRotation(0.5, 1, 0, 0)
+        anim.toValue = CATransform3DMakeRotation(-0.5, 1, 0, 0)
+        anim.duration = 1
+        anim.autoreverses = true
+        anim.repeatCount = 10
+        layer.add(anim, forKey: "transform")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
 
